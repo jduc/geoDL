65ef8182f5f29d73ccfbcd430f79a05f82bdf2ab
jduc <julien.duc.0@gmail.com>
2018-09-25 16:44:52 +0200

    fix setup issue with twine

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
 
         # Indicate who your project is intended for
         'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Scientific/Engineering',
 
 
         # Pick your license as you wish (should match "license" above)
@@ -49,7 +49,7 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2.7.5',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
     ],
 
